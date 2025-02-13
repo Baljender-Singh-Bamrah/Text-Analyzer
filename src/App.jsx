321a49af2132d0d6015e2677eb43c13d8bd6d9d9
Baljender Singh Bamrah <baljendersinghbamrah@gmail.com>
2025-02-13 21:57:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Text_convertor from './component/Text_convertor'
+
+function App() {
+  return (
+    <>
+      <Text_convertor/>
+    </>
+  )
+}
+
+export default App
+
+import React from 'react'
